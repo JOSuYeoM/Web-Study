751b485bafc6d9cb5b5bf31e0748c1a6e12460b1
suyeon <syjo1227@naver.com>
2022-05-29 20:50:17 +0900

    react

--- react/begin-react/src1/MyComponent.js
+++ react/begin-react/src1/MyComponent.js
@@ -1,15 +1,8 @@
-import PropTypes from 'prop-types';
-
-const MyComponent =({name, childern}) => {
-    return(...);
-}
-
-MyComponent.defaultProps ={
-    name:'기본 이름'
+const MyComponent =props =>{
+    return <div>안녕하세요, 제 이름은 {props.name} 입니다. </div>
 };
-
-MyComponent.propTypes={
-    name:PropTypes.string
+MyComponent.defaultProps={
+    name:'기본이름'
 };
 
 export default MyComponent;
