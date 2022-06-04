5a19d7d3cd015c4d839dfe82734f3083d727048d
suyeon <syjo1227@naver.com>
2022-06-05 01:07:20 +0900

    react

--- react/begin-react/src1/App.js
+++ react/begin-react/src1/App.js
@@ -1,7 +1,17 @@
-import Counter from "./Counter";
+import { Component } from "react";
+import ScrollBox from "./ScrollBox";
 
-const App = () => {
-  return <Counter />;
-};
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <ScrollBox ref={(ref)=> this.scrollBox=ref}/>
+        <button onClick={()=> this.scrollBox.ScrollBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
