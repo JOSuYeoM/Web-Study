4ca193324bf28d91e264cad2e9f21b91aebe97bc
suyeon <syjo1227@naver.com>
2022-06-29 15:35:26 +0900

    react

--- react/begin-react/src1/App.js
+++ react/begin-react/src1/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import LifeCycleSample from './LifeCycleSample';
-// 랜덤 색상을 생성합니다.
+import ErrorBoundary from './ErrorBoundary';
+
 function getRandomColor() {
   return '#' + Math.floor(Math.random() * 16777215).toString(16);
 }
@@ -17,7 +18,9 @@ class App extends Component {
     return (
       <div>
         <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
           <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
       </div>
     );
   }
