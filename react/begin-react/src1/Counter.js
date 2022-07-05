995269d94f7dfbd36fe73923a604d8fa6efdbec9
suyeon <syjo1227@naver.com>
2022-07-05 14:28:46 +0900

    react

--- react/begin-react/src1/Counter.js
+++ react/begin-react/src1/Counter.js
@@ -1,29 +1,17 @@
-import { Component } from "react";
+import { useState } from "react";
 
-class Counter extends Component {
-    state={
-            number: 0,
-            fixedNumber:0
-        };
+const Say = () => {
+    const [value, setValue] =useState(0);
 
-    render() {
-        const { number, fixedNumber } = this.state;
-        return (
-            <div>
-                <h1>{number}</h1>
-                <h2>바뀌지 않는 값: {fixedNumber}</h2>
-                <button
-
-                    onClick={() => {
-                        this.setState({ number: number + 1 });
-                    }}
-                >
-                    +1
-
-                </button>
-            </div>
-        );
-    }
-}
+    return(
+        <div>
+            <p>
+                현재 카운터 값은 <b>{value}</b>입니다.
+            </p>
+            <button onClick={()=>setValue(value+1)}>+1</button>
+            <button onClick={()=>setValue(value-1)}>-1</button>
+        </div>
+    );
+};
 
 export default Counter;
